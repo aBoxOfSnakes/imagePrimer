724a34cf1d07d7567f2c3ba9c2ea88011fdf407b
aBoxOfSnakes <40743846+aBoxOfSnakes@users.noreply.github.com>
2018-07-02 22:31:50 -0500

    didn't work, try again tomorrow

--- src/primer/ImagePrime.java
+++ src/primer/ImagePrime.java
@@ -6,22 +6,23 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.math.BigInteger;
-import java.lang.StringBuilder
+import java.lang.StringBuilder;
 
 
 public class ImagePrime {
 
     public static void main(String[] args) throws IOException{
-        File img = new File("feynman.jpg");
+        File imgFile = new File("C:\\Users\\camer\\eclipse-workspace\\imagePrimer\\images\\feynman.jpg");
         BufferedImage img = ImageIO.read(imgFile);
         int[][] nuMatrix = numifyImg(img);
-        StringBuilder bigString = buildString(nuMatrix);
-        BigInteger bigInt = New BigInteger(bigString);
+        String bigString = buildString(nuMatrix);
+        BigInteger bigInt = new BigInteger(bigString);
+        System.out.println(bigInt);
+        System.out.println(nuMatrix);
     }
 
-    public int[][] numifyImg(BufferedImage img) {
-        BufferedImage grImg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
-        int[][] nuMatrix = new int [img.getWidth()][img.getHeight()]
+    public static int[][] numifyImg(BufferedImage img) {
+        int[][] nuMatrix = new int [img.getWidth()][img.getHeight()];
         for (int x=0;x<img.getWidth();x++){
             for (int y=0;y<img.getHeight();y++){
                 Color c = new Color(img.getRGB(x,y));
@@ -32,26 +33,27 @@ public class ImagePrime {
                 int gr = (r+g+b)/3;
 
                 if(gr < 84){
-                    nuMatrix[x][y] = 1
+                    nuMatrix[x][y] = 1;
                 }
                 else if(gr < 169){
-                    nuMatrix[x][y] = 2
+                    nuMatrix[x][y] = 2;
                 }
                 else{
-                    nuMatrix[x][y] = 8
+                    nuMatrix[x][y] = 8;
                 }
             }
         }  
-        return nuMatrix
+        return nuMatrix;
     }
 
-    public StringBuilder buildString(int[][] nuMatrix){
-        StringBuilder bigString = StringBuilder("");
-        for (int x=0;x<nuMatrix.length();x++) {
-            for (int y=0;y<nuMatrix[0].length();y++){
-                bigString.append(nuMatrix[x][y]);
+    public static String buildString(int[][] nuMatrix){
+        StringBuilder bigStringBuilder = new StringBuilder("");
+        for (int x=0;x<nuMatrix.length;x++) {
+            for (int y=0;y<nuMatrix[0].length;y++){
+                bigStringBuilder.append(nuMatrix[x][y]);
             }
         }
+        String bigString = new String(bigStringBuilder);
         return bigString;
     }
 }
